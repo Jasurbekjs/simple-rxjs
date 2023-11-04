22ffa7c0756392c77afabee5910cc3524973dd98
Jasurbek <jasmirzaev@gmail.com>
2023-11-04 17:49:02 +0300

    operator: takeWhile

--- src/operators.js
+++ src/operators.js
@@ -1,13 +1,14 @@
 import { interval } from "rxjs";
-import { filter, map, take, takeLast, tap } from 'rxjs/operators'
+import { filter, map, take, takeLast, takeWhile, tap } from 'rxjs/operators'
 
 const stream$ = interval(1000)
     .pipe(
         tap(v=>console.log('tap: ', v)),
         // map((v)=>v*3),
         // filter(v=> v%2==0 ),
-        take(10),
-        takeLast(5)
+        // take(10),
+        // takeLast(5)
+        takeWhile(v=>v<7)
     )
 
 stream$.subscribe({
