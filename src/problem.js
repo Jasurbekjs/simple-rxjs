df302e242bd316876740b35ebe3553b2cfa6c2e8
Jasurbek <jasmirzaev@gmail.com>
2023-11-04 15:27:19 +0300

    with setInterval

--- src/problem.js
+++ src/problem.js
@@ -11,3 +11,22 @@ const people = [
   {name: 'Irina', age: 23},
   {name: 'Oleg', age: 20}
 ]
+
+btn.addEventListener('click', ()=>{
+  btn.disabled = true
+  let i = 0
+  const agree = []
+
+  const inter = setInterval(()=>{
+    if (people[i]) {
+      if(people[i].age >= 18){
+        agree.push(people[i].name)
+      }
+      display.textContent = agree.join(' ')
+      i++
+    } else {
+      clearInterval(inter)
+      btn.disabled = false
+    }
+  }, 1000)
+})
