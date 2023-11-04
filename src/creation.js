bd7fbfc872626532fb8b7d027aafdf476bdba25a
Jasurbek <jasmirzaev@gmail.com>
2023-11-04 17:05:15 +0300

    Observable

--- src/creation.js
+++ src/creation.js
@@ -1,4 +1,5 @@
-import { of, from } from 'rxjs'
+import { of, from, Observable } from 'rxjs'
+import { scan } from 'rxjs/operators'
 
 // const stream$ = of(1,2,3,4)
 
@@ -6,5 +7,34 @@ import { of, from } from 'rxjs'
 //     console.log(val)
 // })
 
-const arr$ = from([1,2,3,4])
-arr$.subscribe((v)=>console.log(v))
+// const arr$ = from([1,2,3,4]).pipe(scan((acc, v)=>acc.concat(v),[]))
+// arr$.subscribe((v)=>console.log(v))
+
+const stream$ = new Observable(observer => {
+    console.log(observer)
+
+    observer.next("First value")
+
+    setTimeout(()=> observer.next('After 2s'), 2000)
+    setTimeout(()=> observer.complete(), 2500)
+    setTimeout(()=> observer.error('Something went wrong'), 3000)
+    setTimeout(()=> observer.next('After 4s'), 4000)
+})
+
+// stream$.subscribe(
+//     val=>console.log(val),
+//     err=>console.log(err),
+//     ()=>console.log("Complete")
+// )
+
+stream$.subscribe({
+    next (val) {
+        console.log(val)
+    },
+    error (err) {
+        console.log(err)
+    },
+    complete () {
+        console.log("Complete")
+    }
+})
