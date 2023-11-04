598aeb565610f8790642e12cad2f8c3d83c18d31
Jasurbek <jasmirzaev@gmail.com>
2023-11-04 16:05:28 +0300

    stream: from

--- src/creation.js
+++ src/creation.js
@@ -1,7 +1,10 @@
-import { of } from 'rxjs'
+import { of, from } from 'rxjs'
 
-const stream$ = of(1,2,3,4)
+// const stream$ = of(1,2,3,4)
 
-stream$.subscribe((val)=>{
-    console.log(val)
-})
+// stream$.subscribe((val)=>{
+//     console.log(val)
+// })
+
+const arr$ = from([1,2,3,4])
+arr$.subscribe((v)=>console.log(v))
